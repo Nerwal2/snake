700659eb175dc74f8db1040b06a7d35e0cb8bf2f
Nerwal2 <master4567890@gmail.com>
2021-10-14 12:44:27 +0000

    chlorek css

--- src/index.js
+++ src/index.js
@@ -1,96 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.css"
 
-function arraysEqual(arrayOne, arrayTwo){
+function isEqual(arrayOne, arrayTwo) {
+   
     if (arrayOne.length !== arrayTwo.length) {
-        return false;
+      return false;
     }
-    for (var i = 0; i < arrayOne.length; i++){
-        if (arrayOne[i] != arrayTwo[i]){
-            return false;
+  
+    for (var i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] != arrayTwo[i]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridSize: [7, 7],
+            snake: [3, 3],
+            food: [5, 3],
         }
     }
-    return true;
-}
-class Tile extends React.Component {
-    constructor(props){
-    super(props);
-}
-render(){
-    let isFood = arraysEqual(this.props.food, [this.props.X, this.props.Y])
-    ? "food"
-    : "";
-  let isSnake = arraysEqual(this.props.snake, [this.props.X, this.props.Y])
-    ? "snake"
-    : "";
-  let className = `tile ${isSnake} ${isFood}`;
-    return(
-        <div className={className}>
-            ({this.props.X},{this.props.Y})
-        </div>
-    );
-}
-}
 
+    render() {
+        return (
+                <>
+                    <Grid 
+                        grid = {this.props.gameGrid}  
+                        size = {this.state.gridSize}
+                        food = {this.state.food}
+                        snake = {this.state.snake}
+                    />
+                </>
+            )
+    } 
 
+}
 
 class Grid extends React.Component {
-render(){
-    const grid = Array(this.props.size[0]).fill(Array(this.props.size[1]).fill(null));
-    return(
-        <div className="grid">
-            {grid.map((gridX, IndexX) => {
-                return (
-                    <div className="grid-row">
-                        {grid.map((gridY,IndexY) => {
-                            return (
-                                <Tile
-                                X={IndexX}
-                                Y={IndexY}
-                                snake={this.props.snake}
-                                food={this.props.food}
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const gameGrid = Array(this.props.size[0]).fill(Array(this.props.size[1]).fill(null)); 
+            
+            return (
+                <div className="xGrid">
+                    {gameGrid.map((gridY,yIndex) => {
+                        return(
+                        <div className="yGrid">
+                            {gridY.map((gridX,xIndex) => {
+                               return <Tile 
+                                    X = {xIndex} 
+                                    Y = {yIndex} 
+                                    food = {this.props.food}
+                                    snake = {this.props.snake}
                                 />
-                            );
-                        })}
-                    </div>
-                );
-            })}
-        </div>
-    );
-  }
+                            })}
+                        </div>
+                    )}
+                )}
+            </div>        
+        )                     
+    }
 }
-class Game extends React.Component {
-    constructor(props){
-    super(props);
-    this.state = {
-        gridSize: [7,7],
-        snake:[],
-        food:[],
-    };
-}
-getMiddleGrid(gridSize){
-    let xSize = gridSize[0];
-    let ySize = gridSize[1];
-    let xMiddle = parseInt(xSize /2);
-    let yMiddle = parseInt(ySize /2);
-    return [xMiddle,yMiddle];
-}
-ComponentDidMount(){
-    let middleCoordinates = this.getMiddleGrid(this.state.gridSize)
-    this.set.State({snake: middleCoordinates});
-}
-render(){
-    return (
-        <>
-        <Grid
-        size={this.state.gridSize}
-        snake={this.state.snake}
-        food={this.state.food}
-        />
-        </>
-    );
-}
-}
+
+class Tile extends React.Component {
+    constructor(props) {
+        super(props);
+    }    
+
+        render(){
+
+            let foodCordinates = isEqual(this.props.food, [this.props.X, this.props.Y])
+            ? "food"
+            : "";
+
+            let snakeCordiantes = isEqual(this.props.snake, [this.props.X, this.props.Y])
+            ? "snake"
+            : "";
+            
+            let className = `tile ${foodCordinates} ${snakeCordiantes}`;
+
+                return (                          
+                        <div className={className}>
+                            {snakeCordiantes ? "" : ""}
+                            {foodCordinates ? "" : ""}
+                        </div>             
+                )                
+    }
+
     
+
+}
+
+
+
 ReactDOM.render(<Game />, document.getElementById("root"));
